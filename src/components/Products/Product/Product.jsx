c1aede1760243e419aee379b559d15da14b4f469
brebeneladrian <brebenel.adrian@yahoo.com>
2020-12-15 00:56:13 +0100

    Bug fix

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,36 +1,32 @@
 import React from 'react';
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
-
 import useStyles from './styles';
 
-const Product = ({ product, onAddToCart }) => {
-  const classes = useStyles();
+const Product = ({product, onAddToCart}) => {
+    const classes = useStyles();
+    return (
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image={product.media.source} title={product.name}/>
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant='h5' gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        ${product.price.formatted}
+                    </Typography>
+                </div>
+                <Typography dangerouslySetInnerHTML={{ __html:product.description}} variant='body2' color="textSecondary" />
 
-  const handleAddToCart = () => onAddToCart(product.id, 1);
+            </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label="Add to Cart" onClick={()=> onAddToCart(product.id, 1)}>
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActions>
+        </Card>
+    )
+}
 
-  return (
-    <Card className={classes.root}>
-      <CardMedia className={classes.media} image={product.media.source} title={product.name} />
-      <CardContent>
-        <div className={classes.cardContent}>
-          <Typography gutterBottom variant="h5" component="h2">
-            {product.name}
-          </Typography>
-          <Typography gutterBottom variant="h5" component="h2">
-            ${product.price.formatted}
-          </Typography>
-        </div>
-        <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
-      </CardContent>
-      <CardActions disableSpacing className={classes.cardActions}>
-        <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
-          <AddShoppingCart />
-        </IconButton>
-      </CardActions>
-    </Card>
-  );
-};
-
-export default Product;
-
+export default Product
